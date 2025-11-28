b1c73728697e3f7d9db6e10380a36e1c4ffbf002
gokcekk <gokce.kara@motopp.nl>
2025-11-28 09:23:26 +0100

    Added database models and schemas

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
+from .routers import posts
+from .database import engine, Base
+
+# Create tables
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Simple Social Network API")
+
+# Include router
+app.include_router(posts.router)
 
